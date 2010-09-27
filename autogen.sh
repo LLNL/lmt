fea4b6eba1777d8a7423a70a4e63918fc46e6ce6
Jim Garlick <garlick@llnl.gov>
2010-09-27 20:02:05 +0000

    Get configure working again.

---
+++ autogen.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Running aclocal ... "
+aclocal --force -I config
+echo "Running libtoolize ... "
+libtoolize --automake --copy --force
+echo "Running autoheader ... "
+autoheader --force
+echo "Running automake ... "
+automake --copy --add-missing --force
+echo "Running autoconf ... "
+autoconf --force
+echo "Cleaning up ..."
+mv aclocal.m4 config/
+rm -rf autom4te.cache
+echo "Now run ./configure to configure lmt-client-clientss for your environment."
+
