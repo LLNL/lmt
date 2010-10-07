3076f6a8aa96b941cf0849c30845f17f23de2662
Jim Garlick <garlick@llnl.gov>
2010-10-07 20:36:02 +0000

    add DELETE privs to sample grant script

--- scripts/mkusers.sql
+++ scripts/mkusers.sql
@@ -1,12 +1,12 @@
 # Example script for creating LMT MySQL users.
 
 CREATE USER 'lwatchclient';
-GRANT SHOW DATABASES ON *.* TO 'lwatchclient';
-GRANT SELECT ON         *.* TO 'lwatchclient';
+GRANT SHOW DATABASES        ON *.* TO 'lwatchclient';
+GRANT SELECT                ON *.* TO 'lwatchclient';
 
 CREATE USER 'lwatchadmin'@'localhost' IDENTIFIED BY 'mypass';
-GRANT SHOW DATABASES ON *.* TO 'lwatchadmin'@'localhost';
-GRANT SELECT,INSERT  ON *.* TO 'lwatchadmin'@'localhost';
-GRANT CREATE,DROP    ON *.* TO 'lwatchadmin'@'localhost';
+GRANT SHOW DATABASES        ON *.* TO 'lwatchadmin'@'localhost';
+GRANT SELECT,INSERT,DELETE  ON *.* TO 'lwatchadmin'@'localhost';
+GRANT CREATE,DROP           ON *.* TO 'lwatchadmin'@'localhost';
 
 FLUSH PRIVILEGES;
