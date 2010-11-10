525031b244a9c27bfe185a1a45c74de1b127c990
Jim Garlick <garlick@llnl.gov>
2010-11-10 22:01:50 +0000

    Fix memory leak (issue 19), prep for 3.0.2 release

--- libproc/lustre.c
+++ libproc/lustre.c
@@ -356,10 +356,10 @@ _subdirlist (pctx_t ctx, const char *path, List *lp)
     if ((ret = proc_open (ctx, path)) < 0)
         goto done;
     while ((ret = proc_readdir (ctx, PROC_READDIR_NOFILE, &name)) >= 0) {
-        if (strstr (name, "-osc-") && !strstr (name, "MDT"))
-            continue;               /* ignore client-instantiated osc's */
-                                    /*  e.g. lc1-OST0005-osc-ffff81007f018c00 */
-        list_append (l, name);
+        if (strstr (name, "-osc-") && !strstr (name, "MDT")) {
+            free (name);            /* ignore client-instantiated osc's */
+        else                        /*  e.g. lc1-OST0005-osc-ffff81007f018c00 */
+            list_append (l, name);
     }
     if (ret < 0 && errno == 0) /* treat EOF as success */
         ret = 0;
