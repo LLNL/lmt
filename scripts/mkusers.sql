8b321beb81df056e7d8ba0a17a07c735102a95bb
Jim Garlick <garlick@llnl.gov>
2010-12-03 17:39:56 +0000

    Update mkusers.sql per issue 44

--- scripts/mkusers.sql
+++ scripts/mkusers.sql
@@ -1,9 +1,21 @@
 # Example script for creating LMT MySQL users.
+#
+# To improve security, 
+# 1. change the lwatchadmin password!
+# 2. instead of granting to *.*, grant privileges to to filesystem_yourfs.*
+#
+# To run lwatch remotely, add 'lwatchclient'@'hostname' users for each host.
+#
+# Assumes /etc/lmt/lmt.conf contains:
+#   lmt_db_rouser = "lwatchclient"
+#   lmt_db_ropasswd = nil
+CREATE USER 'lwatchclient'@'localhost';
+GRANT SHOW DATABASES        ON *.* TO 'lwatchclient'@'localhost';
+GRANT SELECT                ON *.* TO 'lwatchclient'@'localhost';
 
-CREATE USER 'lwatchclient';
-GRANT SHOW DATABASES        ON *.* TO 'lwatchclient';
-GRANT SELECT                ON *.* TO 'lwatchclient';
-
+# Assumes /etc/lmt/lmt.conf contains:
+#   lmt_db_rwuser = "lwatchadmin"
+#   lmt_db_rwpasswd = "mypass"  -- or use the separate file example
 CREATE USER 'lwatchadmin'@'localhost' IDENTIFIED BY 'mypass';
 GRANT SHOW DATABASES        ON *.* TO 'lwatchadmin'@'localhost';
 GRANT SELECT,INSERT,DELETE  ON *.* TO 'lwatchadmin'@'localhost';
